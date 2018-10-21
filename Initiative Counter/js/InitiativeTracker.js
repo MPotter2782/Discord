b6074b749f7bc0e8686be0321bd16deee3cf81e8
Melinda <mspotte@ilstu.edu>
2018-10-21 15:31:43 -0500

    Adding adventure logs as well as the beginning of the creating of the initiative counter

---
+++ Initiative Counter/js/InitiativeTracker.js
@@ -0,0 +1,6 @@
+var initiative = [];
+for(int i = 0; i <= initiative.length; i++){
+		var initiative[i] = document.getElementByID("Character");
+}
+
+document.getElementById("demo").innerHTML = initiative
