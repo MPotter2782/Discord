1e87f7be7b317a2a849f690544b85a5a3cff0ce6
Melinda <mspotte@ilstu.edu>
2018-10-21 19:58:27 -0500

    Working on the initiative counter

--- Initiative Counter/js/InitiativeTracker.js
+++ Initiative Counter/js/InitiativeTracker.js
@@ -1,6 +1,37 @@
-var initiative = [];
-for(int i = 0; i <= initiative.length; i++){
-		var initiative[i] = document.getElementByID("Character");
-}
+	var chara = [];
+	var init = [];
+	
+	function tryThis(){
+		chara.push(document.getElementById("Character").value);
+		init.push(document.getElementById("Initiative").value);
+		
+		charaText = "<ul>";
+		for (i = 0; i < chara.length; i++) {
+			charaText += "<li>" + chara[i] + "</li>";
+		}
+		charaText += "</ul>";
+		
+		initText = "<ul>";
+		
+		for (i = 0; i < init.length; i++) {
+			initText += "<li>" + init[i] + "</li>";
+		}
+		initText += "</ul>";
+		document.getElementById("demo1").innerHTML = charaText;
+		document.getElementById("demo2").innerHTML = initText;
+	}
 
-document.getElementById("demo").innerHTML = initiative
+		var sec = 0;
+		var round = 0;
+		var min = 0;
+	function trackTime(){
+		sec += 6;
+		round += 1;
+		
+		if(sec == 60){
+			min += 1;
+			sec = 0;
+		}
+		document.getElementById("lengthSec").innerHTML = min + " minutes and " + sec + " seconds";
+		document.getElementById("lengthRound").innerHTML = round + " rounds";
+	}
